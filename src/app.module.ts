a54723435d0e0b743fcd22005c63266cd56a482c
Comprehensive-Wall28 <comprehensive.wall@tuta.com>
2025-11-14 14:43:41 +0200

    Remove incorrectly-placed files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule, ConfigService } from '@nestjs/config'; // Import ConfigService here
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 

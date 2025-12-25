03089933a353940021515ebe8308623fcf1d8a88
Comprehensive-Wall28 <comprehensive.wall@tuta.com>
2025-12-25 23:56:25 +0200

    fix: Fix incorrect import

--- backend/src/common/decorators/roles.decorator.ts
+++ backend/src/common/decorators/roles.decorator.ts
@@ -1,5 +1,5 @@
 import { SetMetadata } from '@nestjs/common';
-import { SystemRole } from '../../employee-subsystem/employee/enums/employee-profile.enums';
+import { SystemRole } from '../../employee-profile/enums/employee-profile.enums';
 
 export const ROLES_KEY = 'roles';
 export const Roles = (...roles: SystemRole[]) => SetMetadata(ROLES_KEY, roles);
